39d87d8f8323f373db9b72f22feefa7993826f2b
Satya Prakash Jha <spjha@modeln.com>
2022-12-15 20:02:05 +0530

    add batch script to launch springboot servers
    Signed-off-by: Satya Prakash Jha <spjha@modeln.com>

---
+++ runservers.bat
@@ -0,0 +1,6 @@
+@ECHO off
+
+start cmd /c ".\gradlew runModelNServer"
+start cmd /c ".\gradlew runManufacturer1Server"
+start cmd /c ".\gradlew runWholesaler1Server"
+start cmd /c ".\gradlew runGPO1Server"
